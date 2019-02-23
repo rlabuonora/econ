25b5920b03d694969ae7307170ce138462098400
Rafa <rlabuonora@yahoo.com>
2019-02-23 10:27:16 -0300

    add bbc theme

--- elasticidad.R
+++ elasticidad.R
@@ -19,7 +19,8 @@ ggplot(demanda_elastica, aes(cantidad, precio)) +
   geom_abline(aes(intercept =  fit$coefficients[["(Intercept)"]], slope = fit$coefficients[["cantidad"]])) +
   geom_point() + 
   geom_rect(mapping=aes(xmin = 0, xmax = cantidad, ymin = 0, ymax = precio), alpha=0.5) + 
-  xlim(0, 4) + 
-  ylim(0, 1100)
+  scale_x_continuous(expand=c(0, 0), limits = c(0, 4)) + 
+  scale_y_continuous(expand=c(0, 0), limits = c(0, 1100)) +
+  bbc_style() 
 
 # chequear extend options!
--- supply-demand.R
+++ supply-demand.R
@@ -1,21 +1,64 @@
 library(dplyr)
 library(ggplot2)
 library(reconPlots)
+library(viridis)
 
 
-line.1 <- data.frame(x = c(1, 9),
-                     y = c(1, 9))
+line.1 <- data.frame(x = c(1, 6),
+                     y = c(1, 6),
+                     name="supply")
 
-line.2 <- data.frame(x = c(9, 1),
-                     y = c(1, 9))
+line.2 <- data.frame(x = c(6, 1),
+                     y = c(1, 6),
+                     name="demand")
+
+line.3 <- line.1 %>% 
+  mutate(y = y + 4,
+         name="demand_1")
 
 line.intersection <- curve_intersect(line.1, line.2)
 
+line.intersection.1 <- curve_intersect(line.3, line.2)
+
+
 
 ggplot(mapping = aes(x=x, y=y)) + 
-  geom_line(data = line.1, color = "red") + 
-  geom_line(data = line.2, color = "blue") + 
+  geom_line(data = line.1, aes(group = 1, color=name)) + 
+  geom_line(data = line.2, aes(group = 1, color=name)) + 
+  geom_line(data = line.3, lty="dashed") + 
   geom_segment(lty = "dashed", 
                aes(x=0, y = line.intersection$y, xend=line.intersection$x, yend=line.intersection$y)) + 
   geom_segment(lty = "dashed", 
-               aes(x=line.intersection$x, y=0, xend=line.intersection$x, yend=line.intersection$y))
+               aes(x=line.intersection$x, y=0, xend=line.intersection$x, yend=line.intersection$y)) + 
+  geom_segment(lty = "dashed",
+               aes(x=line.intersection.1$x, y=0, xend=line.intersection.1$x, yend=line.intersection.1$y)) + 
+  geom_segment(lty = "dashed",
+               aes(x=0, y=line.intersection.1$y, xend=line.intersection.1$x, yend=line.intersection.1$y)) + 
+  annotate("point", x=line.intersection$x, y=line.intersection$y) + 
+  annotate("point", x=line.intersection.1$x, y = line.intersection.1$y) + 
+  ylim(c(0, 7)) + 
+  scale_color_manual( values = c("#23576E", "#099FDB", 
+                                 "#29B00E", "#208F84", 
+                                 "#F55840", "#924F3E"))
+
+
+# pg. 76
+
+ggplot(mapping = aes(x=x, y=y)) + 
+  geom_line(data = line.1, aes(color=name)) + 
+  geom_line(data = line.2, aes(color=name)) + 
+  geom_segment(lty = "dashed", 
+               aes(x=0, y = line.intersection$y, xend=line.intersection$x, yend=line.intersection$y)) + 
+  geom_segment(lty = "dashed", 
+               aes(x=line.intersection$x, y=0, xend=line.intersection$x, yend=line.intersection$y)) +
+  scale_color_manual(name = NULL, 
+                     labels = c("Demanda", "Oferta"),
+                     values = c("#23576E", "#099FDB", 
+                                "#29B00E", "#208F84", 
+                                "#F55840", "#924F3E")) +
+  scale_x_continuous(expand = c(0, 0.2), breaks = c(0, line.intersection$x), labels=c("", expression(~Q[0])))  +
+  scale_y_continuous(expand = c(0, 0.2), breaks = c(0, line.intersection$y), labels=c("", expression(~P[0]))) + 
+  geom_segment(aes(x = 0, y = 0, xend = 0, yend = 6), arrow = arrow(length=unit(0.2, "cm"))) +
+  geom_segment(aes(x=0, y=0, xend = 6, yend = 0), arrow = arrow(length=unit(0.2, "cm"))) + 
+  labs(x=NULL, y=NULL)
+
